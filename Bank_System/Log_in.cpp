c8a5ce6902a6a9d3b6021ae1ad60f914cecb70d0
Tyron Bechayda <tyronbechayda1112@gmail.com>
2023-08-01 21:07:31 +0800

    Added quick sort & binary serch for log in

--- Bank_System/Log_in.cpp
+++ Bank_System/Log_in.cpp
@@ -1,8 +1,16 @@
 #include"functions.h"
 
+size_t partition(std::vector<user::Account>& arr, size_t low, size_t high, int flag);
+void quicksort(std::vector<user::Account>& arr, size_t low, size_t high, int flag);
+bool binary_search(std::vector<user::Account> arr, const std::string target, int flag);
+
 bool BANK::Log_in(std::vector<user::Account>& Data) {
 	std::string input_username = "", input_pass = "";
 	unsigned counter = 1;
+	std::vector<user::Account> sorted_username(Data);
+	quicksort(sorted_username, 0, sorted_username.size() - 1, 0);
+	std::vector<user::Account> sorted_password(Data);
+	quicksort(sorted_password, 0, sorted_password.size() - 1, 1);
 	do {
 		std::cout << "Log In Section" << std::endl;
 		std::cout << "Enter username: ";
@@ -10,16 +18,19 @@ bool BANK::Log_in(std::vector<user::Account>& Data) {
 		std::cout << "Enter a password: ";
 		std::getline(std::cin >> std::ws, input_pass);
 		for (size_t i = 0; i < Data.size(); i++) {
-			if (input_username == Data[i].get_Username() && input_pass == Data[i].get_Password()) return true;
+			if (binary_search(sorted_username, input_username, 0) &&
+				binary_search(sorted_password, input_pass, 1)) return true;
 		}
 		size_t flag = 0;
 		for (size_t i = 0; i < Data.size(); i++) {
-			if (input_username == Data[i].get_Username() && input_pass != Data[i].get_Password()) {
+			if (binary_search(sorted_username, input_username, 0) &&
+				!binary_search(sorted_password, input_pass, 1)) {
 				std::cout << "Wrong password" << std::endl;
 				std::cout << "Please try again" << std::endl;
 				break;
 			}
-			else if (input_username != Data[i].get_Username() && input_pass == Data[i].get_Password()) {
+			else if (!binary_search(sorted_username, input_username, 0) &&
+				binary_search(sorted_password, input_pass, 1)) {
 				std::cout << "Wrong username" << std::endl;
 				std::cout << "Please try again" << std::endl;
 				break;
@@ -38,5 +49,59 @@ bool BANK::Log_in(std::vector<user::Account>& Data) {
 		}
 		counter++;
 	} while (true);
+	return false;
+}
+size_t partition(std::vector<user::Account>& arr, size_t low, size_t high, int flag) {
+	if (flag == 0) {
+		std::string pivot = arr[high].get_Username();
+		size_t i = low - 1;
+
+		for (size_t j = low; j < high; j++) {
+			if (arr[j].get_Username() < pivot) {
+				i++;
+				std::swap(arr[i], arr[j]);
+			}
+		}
+		std::swap(arr[i + 1], arr[high]);
+		return i + 1;
+	}
+	else {
+		std::string pivot = arr[high].get_Password();
+		size_t i = low - 1;
+
+		for (size_t j = low; j < high; j++) {
+			if (arr[j].get_Password() < pivot) {
+				i++;
+				std::swap(arr[i], arr[j]);
+			}
+		}
+		std::swap(arr[i + 1], arr[high]);
+		return i + 1;
+	}
+	return 0;
+}
+void quicksort(std::vector<user::Account>& arr, size_t low, size_t high, int flag) {
+	if (low < high) {
+		size_t pivot = partition(arr, low, high, flag);
+		quicksort(arr, low, pivot - 1, flag);
+		quicksort(arr, pivot + 1, high, flag);
+	}
+}
+bool binary_search(std::vector<user::Account> arr, const std::string target, int flag) {
+	size_t low = 0, high = arr.size() - 1;
+	if(flag == 0)
+		while (low <= high) {
+			size_t mid = low + (high - low) / 2;
+			if (arr[mid].get_Username() == target) return true;
+			else if (arr[mid].get_Username() < target) low = mid + 1;
+			else high = mid - 1;
+		}
+	else
+		while (low <= high) {
+			size_t mid = low + (high - low) / 2;
+			if (arr[mid].get_Password() == target) return true;
+			else if (arr[mid].get_Password() < target) low = mid + 1;
+			else high = mid - 1;
+		}
 	return false;
 }
